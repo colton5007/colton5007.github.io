747ac539238e8d5dee283c253029eb49e3d05f8e
colton5007 <colton.chase.sandvik@gmail.com>
2016-05-05 23:14:49 -0500

    score tracking

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var score = 0;
+var cur = 0;
 function findPos(obj) {
     var curleft = 0, curtop = 0;
     if (obj.offsetParent) {
@@ -28,13 +30,56 @@ img.src = "css/body.png";
 img.crossOrigin = "anonymous";
 
 
-$('#cuerpo').mousemove(function(e) {
+$('#cuerpo').click(function(e) {
     var pos = findPos(this);
     var x = e.pageX - pos.x;
     var y = e.pageY - pos.y;
-    var coord = "x=" + x + ", y=" + y;
     var c = this.getContext('2d');
     var p = c.getImageData(x, y, 1, 1).data; 
     var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-    console.log(coord + "<br>" + hex);
+    check(hex);
 });
+
+function start() {
+	updateScore();
+	nextQuestion();
+}
+
+function check(hex) {
+	if(hex == cur) {
+		score++;
+	} else {
+		score = 0;
+	}
+	updateScore();
+	nextQuestion();
+}
+function updateScore() {
+	$("#score").innerText = score;	
+}
+
+function nextQuestion() {
+	var prev = cur;
+	while(prev == cur) {
+		cur = Math.floor(Math.random() * (1 + 9 - 0));
+	}
+	$("question").innerText = questions[cur].display;
+}
+
+
+function Question(display, hex) {
+	this.display = display;
+	this.hex = hex;
+}
+
+var questions = [new Question("La Cabeza", "#00fffe"),
+new Question("El Cuello", "#d03af8"),
+new Question("La Mano", "#f6ff4a"),
+new Question("El Codo", "#00ccbc"),
+new Question("El Brazo", "#d3186b"),
+new Question("El Pecho", "#29ff5f"),
+new Question("El Estómago", "#233ec6"),
+new Question("Los Dedos de los Pies", "#81045a"),
+new Question("El Pie", "#3a7f52"),
+new Question("La Pierna", "#ff1b20")];
+
