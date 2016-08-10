f8057f93590317af335c5c356659709d7f66c348
colton5007 <colton.chase.sandvik@gmail.com>
2016-08-09 20:39:00 -0500

    Bio

---
+++ bio/js/main.js
@@ -0,0 +1,363 @@
+var cur = 0;
+var score = 0;
+var count = 1;
+var dif = 0;
+var lastTrials = false;
+
+function Choice(correct, words) {
+	this.correct = correct;
+	this.words = words;
+}
+
+var questions = ['a-, an-', 
+'ab-', 
+'-aceous', 
+'-al', 
+'ambi-, amphi-', 
+'amyl-', 
+'archae-', 
+'arthr-', 
+'auto-', 
+'bi-', 
+'bio-', 
+'-blast', 
+'cardi-', 
+'cata-', 
+'-cell-', 
+'centi-', 
+'cephala-', 
+'cervic-', 
+'chloro-',
+'chondr-', 
+'co-', 
+'di-, dipl- (latin)', 
+'dis-', 
+'dors-', 
+'ecto-', 
+'-emia', 
+'en-, endo-, ent-', 
+'enter-', 
+'epi-', 
+'eu-', 
+'ex-', 
+'gastro-', 
+'geo-', 
+'-gen, -gine', 
+'-gene-', 
+'-gest-', 
+'gluc-, glyc-', 
+'gymno-',
+'halo-', 
+'hemi-', 
+'hetro-', 
+'holo-', 
+'homo-', 
+'hydro-', 
+'hyper-', 
+'hypo-', 
+'iso-', 
+'-kary-', 
+'kilo-', 
+'lact-', 
+'-lys, -lyt, -lyst', 
+'macr-', 
+'micro-', 
+'milli-', 
+'mono-',
+'morph-',
+'myc-',
+'necr-',
+'neo-',
+'nuc-',
+'omni-', 
+'oo-', 
+'oste-', 
+'-ped-',
+'phag-', 
+'pheno-', 
+'-phil-', 
+'photo-', 
+'pino-', 
+'-pod', 
+'pro-', 
+'pseudo-', 
+'schis-, schiz-', 
+'-scribe, -script', 
+'semi-', 
+'-sperm-, -spore-', 
+'spir-, -spire', 
+'stat-, -stasis', 
+'stom-, -stome', 
+'sub-',
+'sym-, syn-', 
+'tax-', 
+'telo-', 
+'therm-',
+'trans-',
+'-troph-',
+'vas-',
+'vect-',
+'ventr-',
+'vit-, viv-',
+'zyg-'
+];
+
+var answers = ['no or not', 
+'away from', 
+'of, pertaining to', 
+'having the character of', 
+'both', 
+'starch', 
+'primitive, ancient', 
+'joint', 
+'self', 
+'two', 
+'life', 
+'sprout, bud', 
+'heart', 
+'breakdown', 
+'chamber',
+'hundredth', 
+'head', 'neck', 
+'green', 
+'cartilige', 
+'with, together', 
+'two, double', 
+'apart',
+'back',
+'outside',
+'blood',
+'into, within',
+'intestine, gut',
+'upon, above, over',
+'true',
+'out, away', 
+'stomach', 
+'land, earth', 
+'producer', 
+'origin, birth',
+ 'carry, produce', 
+'sweet', 
+'naked, bare', 
+'salt',
+'half', 
+'different', 
+'whole', 
+'same', 
+'water', 
+'above', 
+'below, under', 
+'equal', 
+'cell nucleus', 
+'thousand', 
+'milk', 
+'decompose, split, burst', 
+'large', 
+'small, millionth', 
+'thousandth',
+'one, single',
+'shape, form',
+'fungus',
+'dead, corpse',
+'new',
+'center',
+'all', 
+'egg', 
+'bone', 
+'foot, child', 
+'eat', 
+'show',
+'loving', 
+'light', 
+'drink', 
+'foot', 
+'before', 
+'false', 
+'split', 
+'write', 
+'half, partly', 
+'seed', 
+'breathe', 
+'standing, staying', 
+'mouth', 
+'under, above', 
+'together', 
+'arrange',
+'end',
+'heat',
+'across',
+'nourishment',
+'vessel',
+'carry',
+'underside',
+'life',
+'joined',
+];
+
+function displayChoice1(choice) {
+	var button1 = document.getElementById("button1");
+	button1.textContent = choice;
+}
+
+function displayChoice2(choice) {
+	var button2 = document.getElementById("button2");
+	button2.textContent = choice;
+}
+
+function displayChoice3(choice) {
+	var button3 = document.getElementById("button3");
+	button3.textContent = choice;
+}
+
+function displayChoice4(choice) {
+	var button4 = document.getElementById("button4");
+	button4.textContent = choice;
+}
+
+function setQuestionStyle(dif) {
+	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var text = document.getElementById("textInput");
+	var submit = document.getElementById("submitButton");
+
+	if (dif == 1) {
+		button1.style.visibility = 'visible';
+		button2.style.visibility = 'visible';
+		button3.style.visibility = 'visible';
+		button4.style.visibility = 'visible';
+		text.style.visibility = 'hidden';
+		submit.style.visibility = 'hidden';
+	} else {
+		button1.style.visibility = 'hidden';
+		button2.style.visibility = 'hidden';
+		button3.style.visibility = 'hidden';
+		button4.style.visibility = 'hidden';
+		text.style.visibility = 'visible';
+		submit.style.visibility = 'visible';
+	}
+}
+
+function getRandomArbitrary(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function displayQuestion() {
+	answerLength = answers.length;
+	var question = questions[cur];
+	var questionDiv = document.getElementById("info");
+	document.getElementById("questionNum").textContent = score;
+	questionDiv.innerHTML = question;
+	setQuestionStyle(dif);
+	if (dif == 1) {
+		var c = [answers[getRandomArbitrary(0, answerLength-1)]];
+		while(c.length < 4) {
+			var temp = answers[getRandomArbitrary(0, answerLength-1)];
+			var x;
+			var y = true;
+			for(x in c) {
+				if(x==temp) {
+					y = false;
+					break;
+				}
+			}
+			if(y) c[c.length] = temp;
+		}
+		var change = true;
+		var z = 0;
+		for(z = 0; z < c.length; z++) {
+			if(c[z] == answers[cur]) change = false;
+		}
+		if(change) c[getRandomArbitrary(0, 3)] = answers[cur];
+		displayChoice1(c[0]);
+		displayChoice2(c[1]);
+		displayChoice3(c[2]);
+		displayChoice4(c[3]);
+		console.log(c);
+	} else {
+		document.getElementById("textBox").value = "";
+	}
+
+}
+
+function displayCorrect(cur) {
+	alert("The correct answer is: \n" + answers[cur-1]);
+}
+
+function checkQuestion(buttonId) {
+	document.getElementById('info').innerHTML = '';
+	cur++;
+	var temp = true;
+	if (buttonId == 5) {
+		var input = document.getElementById("textBox").value.trim();
+		var a = answers[cur-1].split(',');
+		console.log(a);
+		var x;
+		for(x = 0; x < a.length; x++) {	
+			var ans = a[x].trim();
+			console.log(ans + ", " + input);
+			if(ans === input) {
+				score++;
+				
+				displayQuestion();
+				temp = false;
+				break;
+			}
+		}
+		if(temp) {
+			displayCorrect(cur)
+			
+			displayQuestion();
+		}
+		
+	} else if (answers[cur - 1] == document.getElementById('button' + buttonId).innerText) {
+		score++;
+		
+		displayQuestion();
+	} else {
+		displayCorrect(cur);
+		
+		displayQuestion();
+	}
+	if(cur == questions.length) {
+		gameOver();
+	}
+}
+
+function gameOver() {
+	document.getElementById('info').style.visibility='hidden';
+	document.getElementById('questionNum').style.visibility='hidden';
+	document.getElementById('button1').style.visibility='hidden';
+	document.getElementById('button2').style.visibility='hidden';
+	document.getElementById('button3').style.visibility='hidden';
+	document.getElementById('button4').style.visibility='hidden';
+	var end = document.getElementById('end');
+	end.style.visibility='visible';
+	var restartButton = document.getElementById('again');
+	end.textContent="You scored a "+Math.round(score/questions.length*100) + "% on the AP Biology Summer Assignment Vocab Quiz, retry for a better score!";
+	end.appendChild(restartButton);
+}
+
+function restart() {
+	score = 0;
+	cur = 0;
+	document.getElementById("end").style.visibility = "hidden";
+	start();
+}
+
+function start() {
+	document.getElementById('start').style.visibility = 'visible';
+}
+
+function startGame(d) {
+
+	document.getElementById('info').style.visibility = 'visible';
+	document.getElementById('questionNum').style.visibility = 'visible';
+	document.getElementById('start').style.visibility = 'hidden';
+
+	dif = d;
+	displayQuestion();
+}
+
+start();
--- js/main.js
+++ js/main.js
@@ -1,65 +1,204 @@
-var score = 0;
-var cur = 0;
-var cuerpoWidth = 0;
-var cuerpoHeight = 0;
+var current = 1;
+var max = 0;
+function Project(name, infoText, picture) {
+	this.name = name;
+	this.infoText = infoText;
+	this.picture = picture;
 
-function start() {
-	updateScore();
-	nextQuestion();
-	document.getElementById('startButton').style.visibility = 'hidden';
-	var cuerpo = document.getElementById("cuerpo");
-	cuerpoWidth = cuerpo.children[0].clientWidth;
-	cuerpoHeight = cuerpo.children[0].clientHeight;
-	placeEvents();
-}
+	var project = document.createElement('div');
 
+	var width = window.innerWidth * 0.9 + 'px';
+	var height = '600px';
 
-function placeEvents() {
-	for (var i = 0; i < questions.length; i++) {
-		var e = document.createElement("div");
-		e.setAttribute('id', 'part' + i);
-		e.setAttribute('class', 'part');
-		e.setAttribute('onclick', 'check(' + i + ')');
-		e.style.position = "absolute";
-		e.style.left = Math.round(((questions[i].x) / 594) * cuerpoWidth) + 'px';
-		e.style.top = Math.round(((questions[i].y) / 762) * cuerpoHeight) + 'px';
-		e.style.visibility = 'visible';
-		document.getElementById('cuerpo').appendChild(e);
-	}
-}
+	project.style.backgroundColor = '#222222';
 
-function check(id) {
+	project.style.width = width;
+	project.style.height = height;
+
+	var header = document.createElement('h2');
+	var headerContent = document.createTextNode(name);
+	header.style.left = window.innerWidth * 0.60 + "px";
+	header.style.top = "-20px";
+	header.style.color = '#FFFFFF';
+	header.style.backgroundColor = '#222222';
+	header.style.textDecoration = "underline";
+	header.appendChild(headerContent);
+	project.setAttribute('class', 'projContent');
+
+	var info = document.createElement('p');
+	var infoContent = document.createTextNode(infoText);
+	info.appendChild(infoContent);
+	info.style.left = window.innerWidth * 0.60 + "px";
+	info.style.top = "15px";
+	info.style.color = "#CFCFCF";
+	info.style.backgroundColor = '#222222';
+
+	var a = document.createElement('a');
+	a.setAttribute("href", "bio/index.html");
+
+	var image = document.createElement('img');
+	image.setAttribute('height', '600px');
+	image.setAttribute('width', window.innerWidth * 0.6 + 'px');
+	image.setAttribute('src', picture);
+	image.style.zIndex = "-1";
 	
-	if (id == cur) {
-		score++;
+	a.appendChild(image);
+
+	var next = document.createElement('div');
+	next.setAttribute('id', 'projFoward');
+	next.setAttribute('onclick', 'moveProject(+1)');
+	var nextImg = document.createElement('img');
+	nextImg.setAttribute('src', 'img/nextArrow.png');
+	next.appendChild(nextImg);
+	next.style.top = "276px";
+	next.style.left = (window.innerWidth * 0.6) - 64 + 'px';
+	next.style.background = "none";
+	next.style.zIndex = "1000";
+	next.style.width = "64px";
+
+	var back = document.createElement('div');
+	back.setAttribute('id', 'projBack');
+	back.setAttribute('onclick', 'moveProject(-1)');
+	var backImg = document.createElement('img');
+	backImg.setAttribute('src', 'img/backArrow.png');
+	back.appendChild(backImg);
+	back.style.top = "276px";
+	back.style.background = "none";
+	back.style.zIndex = "1000";
+
+	this.div = project;
+	var listItem = document.createElement('li');
+
+	project.appendChild(a);
+	project.appendChild(header);
+	project.appendChild(info);
+	project.appendChild(back);
+	project.appendChild(next);
+	listItem.appendChild(project);
+	document.getElementById('projList').appendChild(listItem);
+	max++;
+}
+
+function moveProject(direction) {
+
+	document.getElementById('projList').childNodes[current].firstChild.setAttribute('class', 'projContent');
+
+	if (current + direction === max + 1) {
+		current = 1;
+		document.getElementById('projList').childNodes[current].firstChild.setAttribute('class', 'displayProj');
+		return;
+	} else if (current + direction === 0) {
+		current = max;
+		document.getElementById('projList').childNodes[current].firstChild.setAttribute('class', 'displayProj');
+		return;
 	} else {
-		score = 0;
+		current += direction;
+		document.getElementById('projList').childNodes[current].firstChild.setAttribute('class', 'displayProj');
+		return;
 	}
-	console.log(score);
-	updateScore();
-	nextQuestion();
+
 }
 
-function updateScore() {
-	document.getElementById('score').children[0].innerHTML = score;
+function setupProjects() {
+	var proj1 = new Project('AP Biology Summer Vocab Quiz', 'Quiz Yourself on the Vocab terms for the AP Bio summer assignment (Click on the image)', 'img/project1.jpg');
+	document.getElementsByClassName('projContent')[0].setAttribute('class', 'displayProj');
 }
 
-function nextQuestion() {
-	var prev = cur;
-	while (prev == cur) {
-		cur = Math.floor(Math.random() * (1 + 9 - 0));
+function setupPage() {
+	this.setText();
+	this.setupProjects();
+	this.checkHide();
+	this.writeAbout();
+	document.getElementById('homeButton').style.top = window.innerHeight - 84 + 'px';
+	document.getElementById('homeButton').style.left = window.innerWidth - 84 + 'px';
+	document.getElementById('c1').style.width = window.innerWidth/2 - 16 + 'px';
+	document.getElementById('c2').style.width = window.innerWidth/2 - 16 + 'px';
+}
+
+function scrollTo(element, to, duration) {
+	var start = element.scrollTop, change = to - start, currentTime = 0, increment = 20;
+
+	var animateScroll = function() {
+		currentTime += increment;
+		var val = Math.easeInOutQuad(currentTime, start, change, duration);
+		element.scrollTop = val;
+		if (currentTime < duration) {
+			setTimeout(animateScroll, increment);
+		}
+	};
+	animateScroll();
+}
+
+//t = current time
+//b = start value
+//c = change in value
+//d = duration
+Math.easeInOutQuad = function(t, b, c, d) {
+	t /= d / 2;
+	if (t < 1)
+		return c / 2 * t * t + b;
+	t--;
+	return -c / 2 * (t * (t - 2) - 1) + b;
+};
+
+function scroll(element, temp) {
+	var y = element.getBoundingClientRect().top;
+	var offset = document.body.getBoundingClientRect().top - y;
+	var i = getScrollMultiplier(element);
+	if (temp === 0) {
+		scrollTo(document.body, y, 1500 * i);
+	}else{
+		scrollTo(document.body, y, 1500 * temp);
 	}
-	document.getElementById('question').children[0].innerHTML = "Toca " + questions[cur].display + ".";
 }
 
-function Question(display, id, x, y) {
-	this.display = display;
-	this.id = id;
-	this.x = x;
-	this.y = y;
+function setText() {
+	var size = Math.round((window.innerWidth * window.innerHeight) / 30000);
+	var nodes = document.body.getElementsByClassName('navButton');
+	for (var i = 0; i < nodes.length; i++) {
+		nodes[i].style.fontSize = size + "px";
+	}
+	
+	var homeTextSize = window.innerHeight / 12;
+	var top = window.innerHeight - Math.round(homeTextSize * 6.8);
+	
+	
+	document.getElementById('homeText').style.fontSize = homeTextSize + "px";
+	document.getElementById('homeText').style.marginTop = top + "px";
+	
+	document.getElementById('titleContent').style.fontSize = Math.round(size * 1.5) + 'px';
+};
+
+function checkHide() {
+	var scrolledOffset = window.scrollY;
+	var button = document.getElementById('homeButton');
+	if (scrolledOffset > 5) {
+		button.style.display = "block";
+	} else {
+		button.style.display = "none";
+	}
+
 }
 
-var questions = [new Question("la cabeza", 0, 287, 159), new Question("el cuello", 1, 287, 232), new Question("la mano", 2, 97, 372), new Question("el codo", 3, 393, 370), new Question("el brazo", 4, 208, 335),
- new Question("el pecho", 5, 287, 300), new Question("el est&oacute;mago", 6, 287, 409), new Question("los dedos de los pies", 7, 192, 702), new Question("el pie", 8, 340, 689),
- new Question("la pierna", 8, 320, 559)];
+function getScrollMultiplier(element) {
+	var scrolledAmount = Math.abs(window.pageYOffset - element.getBoundingClientRect().top);
+	return 0.001 * scrolledAmount;
+}
+
+function Paragraph(content, collumn){
+	var p = document.createElement('p');
+	p.innerHTML = content;
+	document.getElementById('c' + collumn).appendChild(p);
+}
+
+function writeAbout(){
+	var about = document.getElementById('about');
+	new Paragraph("Hello, I am Colton Sandvik, I'm a high school student at Bethany High School. This website is currently pending a lot of changes in the project section.", 1);
+}
+
+function openProj(i) {
+	if(i == 1) {
+		
+	}
+}
 
